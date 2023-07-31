506b8f799569c851d3a02e570256472d5913626c
Jacob Louis <louisjacob14@gmail.com>
2023-07-31 11:17:28 +0530

    feat :added movie api. fix :change static banner content to dynamic

--- frontend/src/Components/Banner/Banner.js
+++ frontend/src/Components/Banner/Banner.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.css'
+import axios from '../../axios'
+import { API_KEY, IMAGE_URL} from '../../Constants/constants'
 
 function Banner() {
+
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+      axios.get(`/trending/all/week?api_key=${API_KEY}&language=en-US`).then(response=>{
+        let results = response.data.results
+        let value = Math.floor(Math.random() * results.length)
+        setMovie(response.data.results[value])
+      })
+    
+    }, [])
+    
+
   return (
-    <div className='banner'>
+    <div
+    style={{backgroundImage: `url(${movie ? IMAGE_URL+movie.backdrop_path : ""})`}}
+    className='banner'>
         <div className="content">
             <div className="title">
-                <h1>Movie Name</h1>
+                <h1>{movie? movie.original_title:''}</h1>
             </div>
             <div className="banner_buttons">
                 <button className='button'>Play</button>
                 <button className='button'>List</button>
             </div>
             <div className="description">
-                <h3>Netflix is an American subscription video on-demand over-the-top streaming service owned and operated by Netflix, Inc.</h3>
+                <h3>{movie? movie.overview:''}</h3>
             </div>
         </div>
         <div className="fade_bottom"></div>
---
+++ frontend/src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'ddd11684348092f06be59a7ee4e91828'
+export const BASE_URL = 'https://api.themoviedb.org/3'
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/original'
