0b82457dd1e9f8574e5c4c1a317f6bf4f56f905b
Jacob Louis <louisjacob14@gmail.com>
2023-08-03 12:45:56 +0530

    fix :display signup in homePage when there is no user logged in

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './NavBar.css'
+import {Link} from 'react-router-dom'
 
 
 function NavBar() {
+  const user = null
   return (
     <div>
         <div className='navbar'>
             <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Logo" />
-            <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
+            {user?<img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" /> :<Link className='btn-link' to='/signup'><button className='avatar-btn'>Signup</button></Link>}
         </div>
     </div>
   )
